c62ec4633492700fc03a2bf490dd5ed7af0d6f7c
Siddhartha Konduru <kondurus@usf.edu>
2024-08-31 09:18:47 -0400

    Task 1 - Employee Information

--- cc_1.js
+++ cc_1.js
@@ -1,1 +1,14 @@
+// Declare variables
+let employeeName = 'Luke Manley'; // string value
+const employeeID = 8591; // number value
+var isActive = true; // boolean value
 
+// Output the values and their types
+console.log('employeeName:', employeeName); // Logs the value of employeeName
+console.log('Type of employeeName:', typeof employeeName); // Logs the type of employeeName
+
+console.log('employeeID:', employeeID); // Logs the value of employeeID
+console.log('Type of employeeID:', typeof employeeID); // Logs the type of employeeID
+
+console.log('isActive:', isActive); // Logs the value of isActive
+console.log('Type of isActive:', typeof isActive); // Logs the type of isActive
