ff9bdd36de96daebc05ce31aadb5708024050cf1
Siddhartha Konduru <kondurus@usf.edu>
2024-08-31 15:08:28 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -12,3 +12,17 @@ console.log('Type of employeeID:', typeof employeeID); // Logs the type of emplo
 
 console.log('isActive:', isActive); // Logs the value of isActive
 console.log('Type of isActive:', typeof isActive); // Logs the type of isActive
+
+// Declare array to store a list of company products
+let products = ["Television", "Calculator", "Smartphone"];
+
+// Declare an object to represent a product with properties: name, price, and inStock
+const productDetails = {
+    name: "Television",
+    price: 189.99,
+    inStock: true
+};
+
+// Log the array and the object to the console
+console.log("Available Products:", products);
+console.log("Product Details:", productDetails);
